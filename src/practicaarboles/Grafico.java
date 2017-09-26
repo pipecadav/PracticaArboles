905900acaad77167d0119bf422d700eef776e0c3
Felipe Cadavid <cadavid.felipe@hotmail.com>
2017-09-26 14:54:16 -0500

    Gráfico

---
+++ src/practicaarboles/Grafico.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaarboles;
+
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Felipe Cadavid  Carolina Diaz
+ */
+public class Grafico extends JPanel{
+    
+    @Override
+    public void paintComponent(Graphics g){
+        // Dibuja el arbol aquí
+        g.drawOval(5, 5, 25, 25);
+        
+    
+
+    }
+    
+  
+    
+    
+}
